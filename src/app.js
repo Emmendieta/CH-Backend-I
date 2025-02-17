f34f633d097be470c15f170028d94e85e14323e3
asrivas <asrivas@issys.gov.ar>
2025-02-17 13:11:14 -0300

    correcciones en el codigo

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ APP.get("/", (req, res) => {
 });
 
 // ********************** PRODUCTOS **********************
+
+//Obtener todos los productos:
 APP.get("/api/products/", async (req, res) => {
         //Establezco un límite de búsqueda en caso de que tenga muchos productos:
         let { limit } = req.query;
@@ -32,6 +34,7 @@ APP.get("/api/products/", async (req, res) => {
         res.send(products);
 });
 
+//Obtener la información de un prodcuto con el id:
 APP.get("/api/products/:pid", async (req, res) => {
         //Obtengo el id por params:
         let {pid} = req.params;
@@ -43,6 +46,45 @@ APP.get("/api/products/:pid", async (req, res) => {
         if(!product) { return res.send(`Error: No se encontró el producto con el id: ${pid}!!!`); }
         //Devuelvo el producto:
         res.send(product);
+});
+
+//Almacenar un producto:
+APP.post("/api/products/", async(req, res) => {
+        const {title, description, code, price, status, stock, category, thumbnails} = req.body;
+        //En caso de que no brinde toda los requerimientos para dar el alta a un nuevo Producto:
+        if (!title || !description || !code || !price || !status ||!stock ||!category || !thumbnails) { return res.send(`Error: Todos los campos son obligatorios para poder proceder a dar el alta del producto!`); }
+        //Verifico que los valores ingresados sean en el formato Correcto:
+        const priceParse = parseFloat(price);
+        if (isNaN(priceParse)) { return res.send(`Error: El precio del producto tiene que ser un valor numérico!!!`); }
+        const statusBool = Boolean(status);
+        const stockParse = parseInt(stock);
+        if (isNaN(stock)) { return res.send(`Error: El stock tiene que ser un valor entero positivo!!!`); }
+        let thumbnailsParse = [];
+        if (Array.isArray(thumbnails)) { thumbnailsParse = thumbnails; }
+        else if (typeof thumbnails === 'string') { thumbnailsParse = thumbnails.split(',').map((thumbnails) => thumbnails.trim()); }
+
+        //Trato de generar el nuevo producto y almacenarlo:
+
+        try {
+                const newProduct = {title, description, code, price, status, stock, category, thumbnails};
+                const product = await PRODUCT_MANAGER.addProduct(newProduct);
+                res.send(product);
+        } catch (error) { res.status(500).send(`Error: No se pudo generar correctamente el producto. + ${error}`); }       
+});
+
+//Eliminar un producto:
+APP.delete("/api/products/:pid", async(req, res) => {
+        const {pid} = req.params;
+        //Parseo el id:
+        pid = parseInt(pid);
+        //Verifico que  se ingrese correctamente el id:
+        if (isNaN(pid)) { return res.send(`Error: el id del producto a eliminar tiene que ser un valor númerico!!!`); }
+        else if(pid <= 0) { return res.send(`Error: El valor numérico del id tiene que ser un número entero positivo mayor a 0!!!`); }
+        //En caso de que est todo ok, lo elimino:
+        let product = await PRODUCT_MANAGER.deleteProduct(pid);
+        if (!product) { return res.send(`Error: no se ha podido eliminar correctamente el podructo con el id ${pid}!!!`); }
+        //En caso de que se elimino correctamente informo que todo se hizo correctamente
+        res.send("Felicidades! se ha eliminado correctamente el producto!");
 });
 
 // ********************** CARRITOS **********************
