43084183257983b1eb7c8e4690372c4fbac048fc
asrivas <asrivas@issys.gov.ar>
2025-03-17 11:48:09 -0300

    codigo provisorio de realTimeProducts

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const PRODUCT_ROUTER = require("./routes/Product.Router.js");
 const CART_ROUTER = require("./routes/Cart.Router.js");
 const VIEW_ROUTER = require("./routes/Views.Router.js");
 
+const { ProductManager } = require("../src/dao/ProductManager.js");
+const PATH_PRODUCT_MANAGER = "./src/data/products.json";
+const PRODUCT_MANAGER = new ProductManager(PATH_PRODUCT_MANAGER);
+
+
 const PORT = 8080;
 const APP = express();
 
@@ -32,14 +37,6 @@ APP.use("/", VIEW_ROUTER);
 
 //Declaro la página de inicio:
 APP.get("/", (req, res) => {
-/*         //DESPUES BORRAR:
-        let testUser = {
-                nombre: "Emiliano",
-                apellido: "Mendieta"
-        }
-        res.render("main", testUser); */
-
-        //VER SI ESTO DESPUES QUEDA:
         res.setHeader('Content-Type','text/plain');
         res.status(200).send('OK');
         
@@ -56,5 +53,45 @@ const SERVER_HTTP = APP.listen(PORT, () => {//Mi servidor HTTP:
 });
 
 const IO = new Server(SERVER_HTTP); //Server webSocket montado sobre serverHTTP
+
+IO.on('connection',async (socket) => {
+        try{
+                console.log('Un cliente se ha conectado!');
+                //Obtengo los productos:
+                const products = await PRODUCT_MANAGER.getProducts();
+                //Recargo la lista de productos:
+                IO.emit('updateProducts', products);
+                //Pongo a escuchar cuando se cree un producto nuevo:
+                socket.on('newProduct', async (product) => {
+                        //Desesctruturo el producto:
+                        const { title, description, code, price, status, stock, category, thumbnails } = product;
+                        //Creo el producto:
+                        await PRODUCT_MANAGER.addProduct(title, description, code, parseFloat(price), Boolean(status), parseInt(stock), category, thumbnails);
+                        //Obtengo los productos:
+                        products = await PRODUCT_MANAGER.getProducts();
+                        //Recargo la lista de productos:
+                        IO.emit('updateProducts', products);
+                });
+        
+                //Escucho cuando se elimina un producto:
+                socket.on('deleteProduct', async (productId) => {
+                        //Elimino el producto:
+                        await PRODUCT_MANAGER.deleteProduct(parseInt(productId));
+                        //Obtengo los productos:
+                        const products = await PRODUCT_MANAGER.getProducts();
+                        //Recargo la lista de productos:
+                        IO.emit('updateProducts', products);
+                });
+        
+                //En caso de que se desconecte el cliente:
+                socket.on('disconnect', () => {
+                        console.log('Se ha desconectado el cliente!');
+                });
+        } catch (error) {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(500).send(`Error: No se pudo generar correctamente el producto. + ${error}`);
+        }
+});
+
 
 
--- src/routes/Views.Router.js
+++ src/routes/Views.Router.js
@@ -12,4 +12,8 @@ router.get('/home', async (req, res) => {
         res.render("home", { products });
 });
 
+router.get('/realtimeproducts', (req, res) => {
+        res.render("realTimeProducts"); 
+});
+
 module.exports = router
